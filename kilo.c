aa2a9b3cebebfc0d5fe23e03f2d3c47ad2234d0d
ayhonz <janmachacek97@gmail.com>
2024-02-10 13:41:29 +0100

    horizontal scroll

--- kilo.c
+++ kilo.c
@@ -41,6 +41,7 @@ struct editorCofig {
     int screenrows;
     int screencols;
     int rowoff;
+    int coloff;
     int numrows;
     erow* row;
     struct termios orig_termios;
@@ -265,6 +266,12 @@ void editorScroll()
     if (E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorDrawRows(struct abuf* ab)
@@ -290,10 +297,12 @@ void editorDrawRows(struct abuf* ab)
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0)
+                len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -315,7 +324,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -338,9 +347,7 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        if (E.cx != E.screencols - 1) {
-            E.cx++;
-        }
+        E.cx++;
         break;
     case ARROW_LEFT:
         if (E.cx != 0) {
@@ -388,6 +395,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
